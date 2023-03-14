ebf12411a69dfb872fa2c892f03a01414d28e90e
AntoJimA <antonio.jaguilera2@gmail.com>
2023-03-14 20:47:07 +0100

    continuacion

--- helloworld.sol
+++ helloworld.sol
@@ -5,10 +5,10 @@ contract game{
     address owner;
     address winner;
     uint256 public bet;
-    address participants;
     bool existGame= false;
-
-
+    uint256 private count = 0;
+    
+    mapping(uint => address[])mapp;
     event GameCreated(address creator,uint gameNumber);
     event GameStarted(address[] participants, uint gameNumber);
 
@@ -18,25 +18,36 @@ contract game{
         owner = msg.sender;
     }
 
-    function CreateGame(address participant) public payable{
+    function createGame(address participant) public payable{
         require(msg.value>0,"not enough ether");
         bet = msg.value;
-        participants = participant;
+        mapp[count].push(msg.sender);
+        mapp[count].push(participant);
         existGame = true;
+        emit GameCreated(msg.sender,count);
+        count++;
 
     }
 
-    function joinGame() public payable{
-        require(msg.sender == participants,"you are not a participant.");
+    function joinGame(uint gameNumber) public payable{
+        require(mapp[gameNumber][1] == msg.sender,"you are not a participant.");
         require(msg.value >= bet, "your bet must be equal or higuer to the existing one.");
         if(msg.value>bet){
             uint balance = msg.value - bet;
             (bool sent,) = payable(msg.sender).call{value:balance}("");
             require(sent, "transaction failed");
             bet = bet * 2;
+        }else{
+            bet+=msg.value;
         }
+        emit GameStarted(mapp[gameNumber], gameNumber);
+
 
     } 
+
+    function makeMove(uint move, uint gameNumber) public payable{
+
+    }
 
 }
 
